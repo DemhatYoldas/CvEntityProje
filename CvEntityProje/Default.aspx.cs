a1244c5753da3e9696ad3fbd9a68b0cbb73541e5
Demhat Yoldaş <64671581+DemhatYoldas@users.noreply.github.com>
2023-03-22 04:13:25 +0300

    güncelleme

--- CvEntityProje/Default.aspx.cs
+++ CvEntityProje/Default.aspx.cs
@@ -9,7 +9,33 @@ namespace CvEntityProje
 {
     public partial class Default : System.Web.UI.Page
     {
+        DBCVENTITYEntities db = new DBCVENTITYEntities();
         protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater1.DataBind();
+            Repeater2.DataSource=db.TBLHAKKIMDA.ToList() ;
+            Repeater2.DataBind();
+            Repeater3.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater3.DataBind();
+            Repeater4.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater4.DataBind();
+            Repeater5.DataSource=db.TBLYETEMEKLER.ToList();
+            Repeater5.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TBLILETISIM t= new TBLILETISIM();
+            t.ADSOYAD = TextBox1.Text;
+            t.MAIL = TextBox2.Text;
+            t.KONU= TextBox3.Text;
+            t.MESAJ=TextBox4.Text;
+            db.TBLILETISIM.Add(t);
+            db.SaveChanges();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
         {
 
         }
